ea4104b7143a1ed35440047b0883ef114f955e4e
Rafael Mejia <113409260+rmeji3@users.noreply.github.com>
2026-02-16 11:29:26 -0600

    Refactor heart rate output format in heartrate.ino

--- lab1/heartrate/heartrate.ino
+++ lab1/heartrate/heartrate.ino
@@ -49,15 +49,15 @@ void loop(){
     // Information from the readBpm function will be saved to our "body"
     // variable.  
     body = bioHub.readBpm();
-  
-    Serial.print(body.heartrate);
-    Serial.println(","); 
-    Serial.print(body.oxygen);
-    Serial.print(",");
+    Serial.print("H:");
+    Serial.print(body.heartRate);
+    Serial.print(",C:");
     Serial.print(body.confidence);
-    Serial.print(","); 
-    Serial.println(body.status); 
-    Serial.println();
+    Serial.print(",O:");
+    Serial.print(body.oxygen);
+    Serial.println(); 
+    // Serial.print("Status: ");
+    // Serial.println(body.status); 
     // tone(BUZZER, 85);
     delay(250); // Slowing it down, we don't need to break our necks here.
 }
