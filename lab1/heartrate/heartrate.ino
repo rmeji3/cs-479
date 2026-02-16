1aa3c514292c62342dbabe61eecb5ee52c6c7475
Rafael Mejia <113409260+rmeji3@users.noreply.github.com>
2026-02-16 11:53:26 -0600

    Comment out sensor startup and configuration logs
    Comment out sensor initialization and configuration messages.

--- lab1/heartrate/heartrate.ino
+++ lab1/heartrate/heartrate.ino
@@ -23,21 +23,21 @@ void setup(){
   
   Wire.begin();
   int result = bioHub.begin();
-  if (!result)
-    Serial.println("Sensor started!");
-  else
-    Serial.println("Could not communicate with the sensor!!!");
+  //if (!result)
+    //Serial.println("Sensor started!");
+  //else
+    //Serial.println("Could not communicate with the sensor!!!");
 
-  Serial.println("Configuring Sensor...."); 
+  //Serial.println("Configuring Sensor...."); 
   int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
-  if(!error){
-    Serial.println("Sensor configured.");
-  }
-  else {
-    Serial.println("Error configuring sensor.");
-    Serial.print("Error: "); 
-    Serial.println(error); 
-  }
+  //if(!error){
+    //Serial.println("Sensor configured.");
+  //}
+  //else {
+    //Serial.println("Error configuring sensor.");
+    //Serial.print("Error: "); 
+    //Serial.println(error); 
+  //}
   // Data lags a bit behind the sensor, if you're finger is on the sensor when
   // it's being configured this delay will give some time for the data to catch
   // up. 
