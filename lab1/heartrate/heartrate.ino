0038cc10e062ba6215a81e523af2da9cb69fea44
Rafael Mejia <113409260+rmeji3@users.noreply.github.com>
2026-02-20 01:38:43 -0800

    Update heartrate.ino

--- lab1/heartrate/heartrate.ino
+++ lab1/heartrate/heartrate.ino
@@ -1,17 +1,5 @@
 #include <SparkFun_Bio_Sensor_Hub_Library.h>
 #include <Wire.h>
-// testing times
-unsigned long startMillis;
-unsigned long currentMillis;
-unsigned long period = 4000;  //the value is a number of milliseconds
-int fakeHR = 40;
-int count = 0;
-int num = 20;
-
-bool testHR = false;
-
-// No other Address options.
-#define DEF_ADDR 0x55
 
 #define BUZZER 12
 
@@ -19,72 +7,68 @@ bool testHR = false;
 const int resPin = 4;
 const int mfioPin = 5;
 
-// Takes address, reset pin, and MFIO pin.
-SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin); 
-
+// Sensor object
+SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin);
 bioData body;
 
-void setup(){
+// timing for serial output
+unsigned long lastPrint = 0;
+const unsigned long printInterval = 250; // 4 times per second
 
+void setup() {
   Serial.begin(115200);
-  startMillis = millis();
+
   pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  
   Wire.begin();
+  delay(1000);
+
   int result = bioHub.begin();
-  //if (!result)
-    //Serial.println("Sensor started!");
-  //else
-    //Serial.println("Could not communicate with the sensor!!!");
-
-  //Serial.println("Configuring Sensor...."); 
-  int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
-  //if(!error){
-    //Serial.println("Sensor configured.");
-  //}
-  //else {
-    //Serial.println("Error configuring sensor.");
-    //Serial.print("Error: "); 
-    //Serial.println(error); 
-  //}
-  // Data lags a bit behind the sensor, if you're finger is on the sensor when
-  // it's being configured this delay will give some time for the data to catch
-  // up. 
-  delay(4000); 
-
+  bioHub.configBpm(MODE_ONE);
 }
-void loop(){
 
-    // Information from the readBpm function will be saved to our "body"
-    // variable.  
-    body = bioHub.readBpm();
-    
-    currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
-    if (currentMillis - startMillis >= period)  //test whether the period has elapsed
-    {
-      testHR = !testHR;
-      fakeHR += num;
-      if(count == 5){
-        count = 0;
-        num = -num;
+void loop() {
+  body = bioHub.readBpm();
+
+  unsigned long now = millis();
+    if (Serial.available()) {
+      char c = Serial.read();
+      if (c == 'B') {
+        digitalWrite(LED_BUILTIN, HIGH);
+        buzzTwice();
+        digitalWrite(LED_BUILTIN, LOW);
       }
-      count++;
-      startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
     }
-    
-   
-    // Serial.print("Status: ");
-    // Serial.println(body.status); 
-    // tone(BUZZER, 85);
-    if(currentMillis - startMillis >= 250){
-      Serial.print("H:");
-      // testHR ? Serial.print(body.heartRate + fakeHR) : Serial.print(body.heartRate);
-      Serial.print(body.heartRate + fakeHR);
-      Serial.print(",C:");
-      Serial.print(body.confidence);
-      Serial.print(",O:");
-      Serial.print(body.oxygen);
-      Serial.println(); 
-    }
+
+  // Send data
+  if (now - lastPrint >= printInterval && body.confidence > 75) {
+    lastPrint = now;
+
+    Serial.print("H:");
+    Serial.print(body.heartRate);
+    Serial.print(",C:");
+    Serial.print(body.confidence);
+    Serial.print(",O:");
+    Serial.print(body.oxygen);
+    Serial.println();
+  }
+
+ 
+}
+
+void buzzTwice() {
+  // passive buzzers are usually louder around 2000–4000 Hz
+  int freq = 2500;
+
+  tone(BUZZER, freq);
+  delay(250);
+  noTone(BUZZER);
+
+  delay(150);
+
+  tone(BUZZER, freq);
+  delay(250);
+  noTone(BUZZER);
 }
